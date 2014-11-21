d2604ba65ef7b40a3e47a35384872260503dd677
Tiago Gashu <tiagogashu@gmail.com>
2014-11-21 14:08:20 -0200

    Usando prototype no lifeController

--- exercicio2/angularLife.js
+++ exercicio2/angularLife.js
@@ -4,13 +4,17 @@ lifeApp.config(function($interpolateProvider){
 });
 
 function lifeController($scope, $timeout) {
-	$scope.rows = 50;
-	$scope.columns = 50;
-	$scope.iterations = 0;
+	var _this = this;
+	_this.scope = $scope;
+	_this.timeout = $timeout;
 
-	var minimumInterval = 50;
+	_this.scope.rows = 50;
+	_this.scope.columns = 50;
+	_this.scope.iterations = 0;
 
-	function getNewGrid() {
+	_this.minimumInterval = 50;
+
+	_this.getNewGrid = function() {
 		var lifeGrid = [];
 		for(var i = 0; i < $scope.rows; i++) {
 			lifeGrid[i] = new Array($scope.columns);
@@ -20,9 +24,9 @@ function lifeController($scope, $timeout) {
 		}
 		return lifeGrid;
 	};
-	$scope.lifeGrid = getNewGrid();
+	_this.scope.lifeGrid = _this.getNewGrid();
 
-	function resetGrid() {
+	_this.resetGrid = function() {
 		$scope.lifeGrid.forEach(function(row) {
 			for(var i = 0; i < row.length; i++) {
 				row[i] = false;
@@ -30,7 +34,7 @@ function lifeController($scope, $timeout) {
 		});
 	};
 
-	function getNeighborsInfo(x, y) {
+	_this.getNeighborsInfo = function(x, y) {
 		var neighbors = [];
 		var neighborsInfo = {'alive' : 0, 'dead' : 0};
 		for(var i = x - 1; i < x + 2; i++) {
@@ -49,8 +53,8 @@ function lifeController($scope, $timeout) {
 		return neighborsInfo;
 	};
 
-	function getNewState(x, y) {
-		var info = getNeighborsInfo(x, y);
+	_this.getNewState = function(x, y) {
+		var info = this.getNeighborsInfo(x, y);
 		// qualquer celula viva com menos de dois ou mais de três vizinho vivos, morre
 		if($scope.lifeGrid[x][y] && (info.alive < 2 || info.alive > 3) ) {
 			return false;
@@ -65,67 +69,95 @@ function lifeController($scope, $timeout) {
 	}
 
 	$scope.toggleState = function(i, j) {
-		if(i >= 0 && i < $scope.rows && j >= 0 && j < $scope.columns) {
-			$scope.lifeGrid[i][j] = !$scope.lifeGrid[i][j];
-		}
+		_this.toggleState(i, j);
 	};
 
 	$scope.step = function($event) {
-		var newGrid = getNewGrid();
-		for(var i = 0; i < $scope.rows; i++) {
-			for(var j = 0; j < $scope.columns; j++) {
-				newGrid[i][j] = getNewState(i, j);
-			}
-		}
-		$scope.lifeGrid = newGrid;
-		$scope.iterations++;
+		_this.step($event);
 	};
 
-
 	$scope.set = function() {
-		var modifiedRows = false;
-		var modifiedColumns = false;
-		if($scope.rowNumber && $scope.rowNumber != $scope.rows) {
-			$scope.rows = $scope.rowNumber;
-			modifiedRows = true;
-		}
-		if($scope.columnNumber && $scope.columnNumber != $scope.columns) {
-			$scope.columns = $scope.columnNumber;
-			modifiedColumns = true;
-		}
-		if(modifiedRows || modifiedColumns) {
-			$scope.lifeGrid = getNewGrid();
-		}
-		$scope.interval = ($scope.intervalNumber >= minimumInterval) ? $scope.intervalNumber : 500;
-		alert('Linhas: ' + $scope.rows + '\nColunas: ' + $scope.columns + '\nIntervalo(ms): ' + $scope.interval);
+		_this.set();
 	};
 
 	$scope.reset = function() {
-		$scope.iterations = 0;
-		resetGrid();
+		_this.reset();
 	};
 
 	$scope.autoStep = function() {
-		if($scope.interval >= minimumInterval) {
-			$scope.step();
-			$scope.timer = $timeout($scope.autoStep, $scope.interval);
-		}
+		_this.autoStep();
 	};
 
 	$scope.auto = function() {
-		if($scope.timer != null) {
-			$timeout.cancel($scope.timer);
-		}
-		if($scope.interval >= minimumInterval) {
-			$scope.step();
-			$scope.timer = $timeout(function() {
-				$scope.autoStep();
-			}, $scope.interval);
-		}
+		_this.auto();
 	};
 
 	$scope.stopAuto = function() {
-		$timeout.cancel($scope.timer);
+		_this.stopAuto();
 	};
 
+}
+
+lifeController.prototype.toggleState = function(i, j) {
+	if(i >= 0 && i < this.scope.rows && j >= 0 && j < this.scope.columns) {
+		this.scope.lifeGrid[i][j] = !this.scope.lifeGrid[i][j];
+	}
+}
+
+lifeController.prototype.step = function($event) {
+	var newGrid = this.getNewGrid();
+	for(var i = 0; i < this.scope.rows; i++) {
+		for(var j = 0; j < this.scope.columns; j++) {
+			newGrid[i][j] = this.getNewState(i, j);
+		}
+	}
+	this.scope.lifeGrid = newGrid;
+	this.scope.iterations++;
+}
+
+lifeController.prototype.set = function() {
+	var modifiedRows = false;
+	var modifiedColumns = false;
+	if(this.scope.rowNumber && this.scope.rowNumber != this.scope.rows) {
+		this.scope.rows = this.scope.rowNumber;
+		modifiedRows = true;
+	}
+	if(this.scope.columnNumber && this.scope.columnNumber != this.scope.columns) {
+		this.scope.columns = this.scope.columnNumber;
+		modifiedColumns = true;
+	}
+	if(modifiedRows || modifiedColumns) {
+		this.scope.lifeGrid = this.getNewGrid();
+	}
+	this.scope.interval = (this.scope.intervalNumber >= minimumInterval) ? this.scope.intervalNumber : 500;
+	alert('Linhas: ' + this.scope.rows + '\nColunas: ' + this.scope.columns + '\nIntervalo(ms): ' + this.scope.intervalNumber);
+}
+
+lifeController.prototype.reset = function() {
+	this.scope.iterations = 0;
+	this.resetGrid();
+}
+
+lifeController.prototype.autoStep = function() {
+	if(this.scope.intervalNumber >= this.minimumInterval) {
+		this.scope.step();
+		this.scope.timer = this.timeout(this.scope.autoStep, this.scope.intervalNumber);
+	}
+}
+
+lifeController.prototype.auto = function() {
+	var _this = this;
+	if(this.scope.timer != null) {
+		this.timeout.cancel(this.scope.timer);
+	}
+	if(this.scope.intervalNumber >= this.minimumInterval) {
+		this.scope.step();
+		this.scope.timer = this.timeout(function() {
+			_this.scope.autoStep();
+		}, this.scope.intervalNumber);
+	}
+}
+
+lifeController.prototype.stopAuto = function() {
+	this.timeout.cancel(this.scope.timer);
 }
